068170d94a09f4e76575a4676195b10fe3e68d02
VinuVicho <vovkamlad@gmail.com>
2021-12-18 12:40:55 +0200

    Initial commit

---
+++ src/main/java/me/vinuvicho/attemptSeven/AttemptSevenApplication.java
@@ -0,0 +1,13 @@
+package me.vinuvicho.attemptSeven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttemptSevenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttemptSevenApplication.class, args);
+	}
+
+}
