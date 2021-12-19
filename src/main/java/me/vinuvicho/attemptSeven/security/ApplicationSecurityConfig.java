e1f6575474b5df485c165231ce5750bd5d70524b
VinuVicho <vovkamlad@gmail.com>
2021-12-20 01:19:47 +0200

    Added password encoder and created first local user (no db)

--- src/main/java/me/vinuvicho/attemptSeven/security/ApplicationSecurityConfig.java
+++ src/main/java/me/vinuvicho/attemptSeven/security/ApplicationSecurityConfig.java
@@ -1,19 +1,45 @@
 package me.vinuvicho.attemptSeven.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "index", "/css/*", "/scripts/test.js").permitAll()            //можна без аутентифікації
+                .antMatchers("/", "index", "/css/*", "/scripts/test.js").permitAll()             //можна без аутентифікації
                 .anyRequest().authenticated().and()
-                .httpBasic();                                                                              //форма зверху
+                .httpBasic();                                                                               //форма зверху
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails userVinuVicho = User.builder()
+                .username("VinuVicho")
+                .password(passwordEncoder.encode("1"))
+                .roles("ADMIN")                                                                             //спрінг розуміє як ROLE_ADMIN
+                .build();
+        return new InMemoryUserDetailsManager(userVinuVicho);
     }
 }
