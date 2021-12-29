3be0b872f9ce18705e0f6f881d2aa0fc3e95174d
VinuVicho <vovkamlad@gmail.com>
2021-12-29 17:27:31 +0200

    Saves User into db

--- src/main/java/me/vinuvicho/attemptSeven/entity/user/UserService.java
+++ src/main/java/me/vinuvicho/attemptSeven/entity/user/UserService.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 public class UserService implements UserDetailsService {
 
     private final UserDao userDao;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -25,8 +27,12 @@ public class UserService implements UserDetailsService {
         if (userExists) {
             throw new IllegalStateException("Username or Email is already taken");
         }
+                //Steal password here))
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        userDao.save(user);
             //TODO зупинився тут
 
-        return null;
+        return "yes?";
     }
 }
