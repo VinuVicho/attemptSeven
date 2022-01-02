61f8a677f98daeadd7066744619b55dd83ac1f69
VinuVicho <vovkamlad@gmail.com>
2022-01-02 22:34:03 +0200

    -unused comment

--- src/main/java/me/vinuvicho/attemptSeven/AttemptSevenApplication.java
+++ src/main/java/me/vinuvicho/attemptSeven/AttemptSevenApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync			//prob not working
+@EnableAsync
 public class AttemptSevenApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AttemptSevenApplication.class, args);
