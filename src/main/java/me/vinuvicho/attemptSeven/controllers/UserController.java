047676af0cc4c18a73b50a29764df290e2349b47
VinuVicho <vovkamlad@gmail.com>
2022-01-02 22:24:48 +0200

    Email Validator

--- src/main/java/me/vinuvicho/attemptSeven/controllers/UserController.java
+++ src/main/java/me/vinuvicho/attemptSeven/controllers/UserController.java
@@ -1,26 +1,10 @@
 package me.vinuvicho.attemptSeven.controllers;
 
-import me.vinuvicho.attemptSeven.entity.user.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
-import java.util.List;
 
 @RestController
 @RequestMapping("user")
 public class UserController {       //TODO: UserController
 
-    private static final List<User> USERS = Arrays.asList(
-//            new User(1, "VinuVicho"),
-//            new User(2, "Kodlon"),
-//            new User(3, "Vouzze")
-    );
-
-//    @GetMapping("/{userId}")
-//    public User getUser(@PathVariable("userId") Integer userId) {
-//        return USERS.stream()
-//                .filter(user -> userId.equals(user.getUserId())).findFirst()
-//                .orElseThrow(() -> new IllegalStateException("no user with " + userId + " id"));
-//    }
 }
--- src/main/java/me/vinuvicho/attemptSeven/registration/Validate.java
+++ src/main/java/me/vinuvicho/attemptSeven/registration/Validate.java
@@ -1,9 +1,18 @@
 package me.vinuvicho.attemptSeven.registration;
 
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
 public class Validate {
 
-    public boolean validateEmail(String s) {
-        //TODO: make validation
-        return true;
+    public boolean validateEmail(String email) {
+        boolean result = true;
+        try {
+            InternetAddress emailAddress = new InternetAddress(email);
+            emailAddress.validate();
+        } catch (AddressException ex) {
+            result = false;
+        }
+        return result;
     }
 }
