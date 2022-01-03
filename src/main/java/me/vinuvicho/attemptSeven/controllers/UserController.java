54f28203059113648600231e9d0adbd5efe4a8e4
VinuVicho <vovkamlad@gmail.com>
2022-01-03 03:42:47 +0200

    removed TODO

--- src/main/java/me/vinuvicho/attemptSeven/controllers/UserController.java
+++ src/main/java/me/vinuvicho/attemptSeven/controllers/UserController.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("user")
-public class UserController {       //TODO: UserController
+public class UserController {
 
 }
