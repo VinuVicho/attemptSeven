076f4469b0fd278471f16a19301f5298a1424c47
VinuVicho <vovkamlad@gmail.com>
2022-01-06 03:58:59 +0200

    removed chat (for now)

---
+++ src/main/java/me/vinuvicho/attemptSeven/entity/chat/chat.java
@@ -0,0 +1,13 @@
+package me.vinuvicho.attemptSeven.entity.chat;
+
+
+//@Getter
+//@Setter
+//@Entity
+//@ToString
+public class chat {         //TODO chat
+//    @SequenceGenerator(name = "post_sequence", sequenceName = "post_sequence", allocationSize = 1)
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "post_sequence")
+    private Long id;
+}
