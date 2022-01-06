0a6c5237fc04073496dab69c3c4731af7204a8fd
VinuVicho <vovkamlad@gmail.com>
2022-01-06 06:03:01 +0200

    optimize imports

--- src/main/java/me/vinuvicho/attemptSeven/entity/chat/chat.java
+++ src/main/java/me/vinuvicho/attemptSeven/entity/chat/chat.java
@@ -1,19 +1,18 @@
 package me.vinuvicho.attemptSeven.entity.chat;
 
-
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-import javax.persistence.*;
-
-@Getter
-@Setter
-@Entity
-@ToString
+//import lombok.Getter;
+//import lombok.Setter;
+//import lombok.ToString;
+//
+//import javax.persistence.*;
+//
+//@Getter
+//@Setter
+//@Entity
+//@ToString
 public class chat {         //TODO chat
-    @SequenceGenerator(name = "post_sequence", sequenceName = "post_sequence", allocationSize = 1)
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "post_sequence")
-    private Long id;
+//    @SequenceGenerator(name = "post_sequence", sequenceName = "post_sequence", allocationSize = 1)
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "post_sequence")
+//    private Long id;
 }
