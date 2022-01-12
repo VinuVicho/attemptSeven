837d82b0fdb0497026a0526d71a895c81724332d
VinuVicho <vovkamlad@gmail.com>
2022-01-12 02:53:24 +0200

    Register page normal

--- src/main/java/me/vinuvicho/attemptSeven/controllers/TestController.java
+++ src/main/java/me/vinuvicho/attemptSeven/controllers/TestController.java
@@ -1,7 +1,5 @@
 package me.vinuvicho.attemptSeven.controllers;
 
-
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
