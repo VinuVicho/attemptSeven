59cc3ffeb684df152c176abe308f6f15ba7b86a9
VinuVicho <vovkamlad@gmail.com>
2022-01-19 06:38:52 +0200

    Validator base created

--- src/main/java/me/vinuvicho/attemptSeven/additional/Validator.java
+++ src/main/java/me/vinuvicho/attemptSeven/additional/Validator.java
@@ -8,18 +8,27 @@ import javax.mail.internet.InternetAddress;
 public class Validator {
 
     public boolean validateRegistrationRequest(RegistrationRequest request) {
+        return validateEmail(request.getEmail()) &&
+                validateUsername(request.getUsername()) &&
+                validatePassword(request.getPassword());
+    }
 
+
+    public boolean validateUsername(String username) {
 
         return true;
     }
-
     public boolean validateEmail(String email) {
         try {
             InternetAddress emailAddress = new InternetAddress(email);
             emailAddress.validate();
+            return true;
         } catch (AddressException ex) {
             return false;
         }
+    }
+    public boolean validatePassword(String password) {
+
         return true;
     }
 }
